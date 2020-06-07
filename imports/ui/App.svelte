d4da3b5cad60f5abdfd3d0f97b00ac81ade62fdf
Subhasish <subhasishdas159@gmail.com>
2020-06-07 13:26:12 +0530

    simplified example

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,66 +1,62 @@
 <script>
   import { Meteor } from 'meteor/meteor'
   import { useTracker } from 'meteor/rdb:svelte-meteor-data';
-  import { Tasks } from '../api/tasks.js'
+  import { Heroes } from '../api/heroes.js'
   import { BlazeTemplate }   from 'meteor/svelte:blaze-integration'
   import { onMount } from 'svelte'
-  import Task from './Task.svelte'
+  import Hero from './Hero.svelte'
+  import { selectedHero } from '../api/store.js'
 
-  let newTask = ""
-  let hideCompleted = false
-  let tasks
+  let newHero = ""
   let currentUser
 
   onMount(async () => {
-    Meteor.subscribe('tasks')
+    await Meteor.subscribe('publishedHeroes')
   })
 
-  const taskStore = Tasks.find({}, { sort: { createdAt: -1 } })
-  $: incompleteCount = useTracker(() => Tasks.find({ checked: { $ne: true } }).count())
+    $: heroes = useTracker(() => Heroes.find({}, { sort: { createdAt: -1 } }).fetch());
 
-  $: currentUser = useTracker(() => Meteor.user())
-
-  $: {
-      tasks = $taskStore
-      if (hideCompleted) {
-          tasks = tasks.filter(task => !task.checked)
+    const handleSubmit = (event) => {
+      Meteor.call('heroes.insert', newHero)
+      newHero = ""
+    }
+    $:{
+      currentUser = useTracker(() => Meteor.user())
+      if($currentUser) {
+        heroes = useTracker(() => Heroes.find({ username: $currentUser.username }, { sort: { createdAt: -1 } }).fetch());
       }
-  };
-  const handleSubmit = (event) => {
-    Meteor.call('tasks.insert', newTask)
-    newTask = ""
-  }
+    }
+
+    $: {
+        if($currentUser) {
+        console.log($currentUser.username)
+      }
+    }
 </script>
  
  
 <div class="container">
   <header>
-    <h1>Todo List ({ $incompleteCount })</h1>
-
-    <label className="hide-completed">
-      <input type="checkbox"
-      bind:checked={hideCompleted}
-      >
-      Hide Completed Tasks
-    </label>
+    <h1>Todo List</h1>
     <br>
 
     <BlazeTemplate template="loginButtons" />
 
     {#if $currentUser}
 
-      <form class="new-task" on:submit|preventDefault={handleSubmit}>
-        <input type="text" placeholder="Type to add new tasks" bind:value="{newTask}" />
+      <form on:submit|preventDefault={handleSubmit}>
+        <input type="text" placeholder="Type to add a new hero" bind:value="{newHero}" />
       </form>
+      {$currentUser.username}
 
     {/if}
 
   </header>
   <ul>
-  {#each tasks as task (task._id)}
-    <Task
-      key={task._id}
-      task={task}
+  {#each $heroes as hero (hero._id)}
+    <Hero
+      key={hero._id}
+      hero={hero}
     />
   {/each}
   </ul>
