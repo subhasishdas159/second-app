e7e80c4cb4b85199f149c1da9dc3bb5591250d3d
Subhasish <subhasishdas159@gmail.com>
2020-06-13 23:51:58 +0530

    confused commit 2

---
+++ imports/ui/pages/Dashboard.svelte
@@ -0,0 +1,9 @@
+<h3>Dashboard</h3>
+
+<a href="#/tags">tags</a>
+<br>
+<a href="#/manage-tags">manage-tags</a>
+<br>
+<a href="#/team">team</a>
+<br>
+<a href="#/profile">profile</a>
---
+++ imports/ui/pages/Team.svelte
@@ -0,0 +1,14 @@
+<script>
+	import {querystring} from 'svelte-spa-router'
+	import qs from 'simple-query-string'
+ 
+	var parsed = qs.parse($querystring);
+	 
+	console.log(parsed["teamId"]);
+	console.log(parsed["abc"]);
+
+</script>
+
+<a href="#/">Back to Dashboard</a>
+
+<h3>Team page</h3>
